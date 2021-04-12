4ec8b6315b7e669bb6f70c445d69fe899e1db4ed
Kratos098 <manibhaskaran22@gmail.com>
2021-04-12 13:55:53 -0700

    added tabs for tables

--- javascript/create-table.js
+++ javascript/create-table.js
@@ -1,38 +1,74 @@
-function Upload() {
-    var fileUpload = document.getElementById("data_file");
-    document.getEleme
+function UploadFile(type) {
+    // console.log(type);
+    var fileUpload = type == 1 ? document.getElementById("data_file") : document.getElementById("data_log");
+    // console.log(fileUpload);
     var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.csv|.txt)$/;
     if (regex.test(fileUpload.value.toLowerCase())) {
-        if (typeof (FileReader) != "undefined") {
-            var reader = new FileReader();
-            reader.onload = function (e) {
-                var table = document.createElement("table");
-                var rows = e.target.result.split("\n");
-                for (var i = 0; i < rows.length; i++) {
-                    var cells = rows[i].split(",");
-                    if (cells.length > 1) {
-                        var row = table.insertRow(-1);
-                        for (var j = 0; j < cells.length; j++) {
-                            var cell = row.insertCell(-1);
-                            cell.innerHTML = cells[j];
-                        }
-                    }
-                    //console.log(i);
-                    // updateProgress(i, rows.length);
-                }
-                // updateProgress(0, rows.length);
-                var dvCSV = document.getElementById("table");
-                dvCSV.innerHTML = "";
-                dvCSV.appendChild(table);
-            }
-            
-            reader.readAsText(fileUpload.files[0]);
-        } else {
-            alert("This browser does not support HTML5.");
-        }
-    } else {
+        CreateTable(null, type);
+    } 
+    else {
         alert("Please upload a valid CSV file.");
     }
+}
+
+function CreateTable(evt, type) {
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    
+    var fileUpload = type == 1 ? document.getElementById("data_file") : document.getElementById("data_log");
+    if(fileUpload.value == "") {
+        // console.log("ending");
+        alert("Please upload a file!")
+        return;
+    }
+    else if(evt == null || evt == undefined) {
+        return;
+    }
+
+    var dvTBL = type == 1 ? document.getElementById("data_table") : document.getElementById("log_table");
+    dvTBL.style.display = "table";
+    evt.target.className += " active";
+    if (typeof (FileReader) != "undefined") {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            var table = document.createElement("table");
+            var rows = e.target.result.split("\n");
+            for (var i = 0; i < rows.length; i++) {
+                var cells = rows[i].split(",");
+                if (cells.length > 1) {
+                    var row = table.insertRow(-1);
+                    for (var j = 0; j < cells.length; j++) {
+                        var cell = row.insertCell(-1);
+                        cell.innerHTML = cells[j];
+                    }
+                }
+                // console.log(i);
+                // updateProgress(i, rows.length);
+            }
+            // updateProgress(0, rows.length);
+            var dvCSV = type == 1 ? document.getElementById("data_table_scroller") : document.getElementById("log_table_scroller");
+            dvCSV.innerHTML = "";
+            dvCSV.appendChild(table);
+        }
+        reader.readAsText(fileUpload.files[0]);
+    } 
+    else {
+        alert("This browser does not support HTML5.");
+        return
+    }
+
+    var button = type == 1 ? document.getElementById("data_table_button") : document.getElementById("log_table_button")
+    button.click();
 }
 
 // function updateProgress(row, length) {
